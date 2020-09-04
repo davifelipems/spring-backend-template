799b2c2e2f37ee845154c97dd4aefdc5fd6b3f26
Davi Menezes <davifelipems@hotmail.com>
2020-09-04 19:07:31 -0300

    Frist Commit

---
+++ src/main/java/com/br/davifelipe/springjwt/SpringJwtBackendTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.br.davifelipe.springjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtBackendTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtBackendTemplateApplication.class, args);
+	}
+
+}
