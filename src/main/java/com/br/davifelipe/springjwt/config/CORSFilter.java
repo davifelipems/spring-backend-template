a07aeb975e65205dd24e7af035c09db4bd78d52c
Davi Menezes <davifelipems@hotmail.com>
2020-10-22 08:56:55 -0300

    CORS Authorization preflight

--- src/main/java/com/br/davifelipe/springjwt/config/CORSFilter.java
+++ src/main/java/com/br/davifelipe/springjwt/config/CORSFilter.java
@@ -28,7 +28,7 @@ public class CORSFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
 		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+		response.setHeader("Access-Control-Allow-Headers", "Authorization,Content-Type, Accept, X-Requested-With, remember-me");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
